ca91b0eda2911fc91c4b94e06d5631a5009eaa2d
Fingbel <31929378+Fingbel@users.noreply.github.com>
2022-01-10 01:27:37 +0100

    Refactoring

--- media/lua/client/Smokey.lua
+++ media/lua/client/Smokey.lua
@@ -12,6 +12,8 @@ local function LightCigOnOven(_player, _context, _worldObjects, _test)
 	
 	--Check for cigarette stock
 	if inventory:containsType('Cigarettes') then
+		
+		--We have cigarette, let's see if we have a source of flame where we clicked
 		for i,stove in ipairs(_worldObjects) do
 			
 			--did we clicked a lit  stove which is not a microwave?
@@ -30,17 +32,22 @@ local function LightCigOnOven(_player, _context, _worldObjects, _test)
 			elseif instanceof(stove, "IsoObject") and stove:getSpriteName() == "camping_01_5" then
 				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), player, OnSmoking, stove, cigarette)
 				
-				--DEBUG
-				--print(stove:getSpriteName())
 			end
 		end		
 	end
 end
 
-function OnSmoking (player, stove, cigarette)
+local function DoWeHaveCigsInBags(inventory)
 
-	if luautils.walkAdj(player, stove:getSquare(), false) then
-		ISTimedActionQueue.add(ISSmoking:new(player, stove, cigarette, 460))
+end
+
+
+
+
+local function OnSmoking (_player, _stove,_cigarette)
+
+	if luautils.walkAdj(_player, _stove:getSquare(), false) then
+		ISTimedActionQueue.add(ISSmoking:new(player, stove,_cigarette, 460))
 	end
 	
 end
