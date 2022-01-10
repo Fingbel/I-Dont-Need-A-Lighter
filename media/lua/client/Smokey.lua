ee4f8a18857526a3cf0540ec7b0a9c201e7457b1
Fingbel <fingbel@gmail.com>
2022-01-10 07:37:36 +0100

    Update Smokey.lua

--- media/lua/client/Smokey.lua
+++ media/lua/client/Smokey.lua
@@ -6,47 +6,45 @@ local Smokey = {}
 local function LightCigOnOven(_player, _context, _worldObjects, _test)
 
 	local player = getSpecificPlayer(_player)
-	local stats = player:getStats()
 	local inventory = player:getInventory()
 	local cigarette = inventory:getItemFromType("Base.Cigarettes")
-	local sac = player:getClothingItem_Back()
 		
 	
 		--Check for cigarette stock
-	if inventory:containsType('Cigarettes') then
-		
+	if inventory:containsType('Cigarettes') then	
 		--We have cigarette, let's see if we have a source of flame where we clicked
 		for i,stove in ipairs(_worldObjects) do
-		print(stove:getName())
+
 			--did we clicked a lit  stove which is not a microwave?
 			if instanceof(stove, 'IsoStove') and stove:Activated() and not 	stove:isMicrowave() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithOven'), player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithOven'), _player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a lit fireplace ?
 			elseif instanceof(stove,'IsoFireplace') and stove:isLit() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithFireplace'), player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithFireplace'), _player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a lit barbecue ?
 			elseif instanceof(stove,'IsoBarbecue') and stove:isLit() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithBarbecue'), player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithBarbecue'), _player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a Campfire ? We check the sprite directly to check if the campfire is lit or not
 			elseif instanceof(stove, "IsoObject") and stove:getSpriteName() == "camping_01_5" then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), player, OnStoveSmoking, stove, cigarette)
-				
+				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), _player, OnStoveSmoking, stove, cigarette)
+			end	
+			
 			--did we clicked on a Fire ? You mad man
-			elseif instanceof(stove, "IsoFire") and not object:isPermanent() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithFire'), player, OnStoveSmoking, stove, cigarette)	
-				print("FIRE ALERT!")
-			end
+			-- elseif instanceof(stove, "IsoFire") and not object:isPermanent() then
+				-- _context:addOption(getText('ContextMenu_LightCigaretteWithFire'), player, OnStoveSmoking, stove, cigarette)	
+				-- print("FIRE ALERT!")
+			--end
 		end		
 	end
 end
 
 Events.OnFillWorldObjectContextMenu.Add(LightCigOnOven)
 
-local function OnStoveSmoking (_player, _stove,_cigarette)
+function OnStoveSmoking (_player, _stove, _cigarette)
 	if luautils.walkAdj(_player, _stove:getSquare(), false) then
-		ISTimedActionQueue.add(ISSmoking:new(player, stove,_cigarette, 460))
+		ISTimedActionQueue.add(IsStoveSmoking:new(player, stove,_cigarette, 460))
 	end
 end
