db8bfef7876e92e6f2c10a83229cdcd5b30727d0
Fingbel <fingbel@gmail.com>
2022-01-10 01:01:06 +0100

    Updated translations
    Added PTBR

--- media/lua/client/Smokey.lua
+++ media/lua/client/Smokey.lua
@@ -1,6 +1,6 @@
---RealSmoker Mod by Fingbel
+--NoLighterNeeded Mod by Fingbel
 
-RealSmoker = RealSmoker
+NoLighterNeeded = NoLighterNeeded
 local Smokey = {}
 
 local function LightCigOnOven(_player, _context, _worldObjects, _test)
@@ -10,18 +10,23 @@ local function LightCigOnOven(_player, _context, _worldObjects, _test)
 	local inventory = player:getInventory()
 	local cigarette = inventory:getItemFromType("Base.Cigarettes")
 	
+	--Check for cigarette stock
 	if inventory:containsType('Cigarettes') then
 		for i,stove in ipairs(_worldObjects) do
 			
+			--did we clicked a lit  stove which is not a microwave?
 			if instanceof(stove, 'IsoStove') and stove:Activated() and not 	stove:isMicrowave() then
 				_context:addOption(getText('ContextMenu_LightCigaretteWithOven'), player, OnSmoking, stove, cigarette)
 			
+			--did we clicked a lit fireplace ?
 			elseif instanceof(stove,'IsoFireplace') and stove:isLit() then
 				_context:addOption(getText('ContextMenu_LightCigaretteWithFireplace'), player, OnSmoking, stove, cigarette)
-
+			
+			--did we clicked a lit barbecue ?
 			elseif instanceof(stove,'IsoBarbecue') and stove:isLit() then
 				_context:addOption(getText('ContextMenu_LightCigaretteWithBarbecue'), player, OnSmoking, stove, cigarette)
 			
+			--did we clicked a Campfire ?
 			elseif instanceof(stove, "IsoObject") and stove:getName() == "Campfire" then
 				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), player, OnSmoking, stove, cigarette)
 			end
