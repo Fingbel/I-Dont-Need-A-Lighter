6419200c05cc8a675bbd7932b7ae94b3ce85e5b2
Fingbel <fingbel@gmail.com>
2022-01-10 07:54:38 +0100

    Update Smokey.lua

--- media/lua/client/Smokey.lua
+++ media/lua/client/Smokey.lua
@@ -17,19 +17,19 @@ local function LightCigOnOven(_player, _context, _worldObjects, _test)
 
 			--did we clicked a lit  stove which is not a microwave?
 			if instanceof(stove, 'IsoStove') and stove:Activated() and not 	stove:isMicrowave() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithOven'), _player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithOven'), player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a lit fireplace ?
 			elseif instanceof(stove,'IsoFireplace') and stove:isLit() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithFireplace'), _player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithFireplace'), player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a lit barbecue ?
 			elseif instanceof(stove,'IsoBarbecue') and stove:isLit() then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithBarbecue'), _player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithBarbecue'), player, OnStoveSmoking, stove, cigarette)
 			
 			--did we clicked a Campfire ? We check the sprite directly to check if the campfire is lit or not
 			elseif instanceof(stove, "IsoObject") and stove:getSpriteName() == "camping_01_5" then
-				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), _player, OnStoveSmoking, stove, cigarette)
+				_context:addOption(getText('ContextMenu_LightCigaretteWithCampFire'), player, OnStoveSmoking, stove, cigarette)
 			end	
 			
 			--did we clicked on a Fire ? You mad man
@@ -41,10 +41,10 @@ local function LightCigOnOven(_player, _context, _worldObjects, _test)
 	end
 end
 
-Events.OnFillWorldObjectContextMenu.Add(LightCigOnOven)
-
 function OnStoveSmoking (_player, _stove, _cigarette)
 	if luautils.walkAdj(_player, _stove:getSquare(), false) then
 		ISTimedActionQueue.add(IsStoveSmoking:new(player, stove,_cigarette, 460))
 	end
 end
+
+Events.OnFillWorldObjectContextMenu.Add(LightCigOnOven)
