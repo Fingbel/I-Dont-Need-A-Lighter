4ada1f4addd16d5b3d2735dd96ce9a164841a451
Fingbel <fingbel@gmail.com>
2022-08-17 08:51:08 +0200

    Revert "update"
    This reverts commit 4b312e89f2727948d3af5129db4f9e761a6d2f44.

--- Contents/mods/I Don't Need A Lighter/media/lua/shared/IDNALUtils.lua
+++ Contents/mods/I Don't Need A Lighter/media/lua/shared/IDNALUtils.lua
@@ -18,9 +18,9 @@ function CheckInventoryForCigarette(player)
 	--Do we have smokable in our pocket
 	for i=0, inventoryItems:size()-1 do			
 		if inventoryItems:get(i):getEatType() ==  ('Cigarettes') or inventoryItems:get(i):getEatType() == ('CigarettesOne') then
-			if inventoryItems:get(i):getDisplayName() ~= ('Candy Cigarette') then
-				smokable[getTableSize(smokable)] = inventoryItems:get(i)
-			end
+			
+			--If yes we add them to the smokable array 
+			smokable[getTableSize(smokable)] = inventoryItems:get(i)
 		end	
 	end
 
@@ -32,9 +32,7 @@ function CheckInventoryForCigarette(player)
 			local ContainerContent = inventoryItems:get(i):getItemContainer():getItems()				
 			for i=0, ContainerContent:size()-1 do				
 				if ContainerContent:get(i):getEatType() ==  ('Cigarettes') or ContainerContent:get(i):getEatType() == ('CigarettesOne')  then
-					if inventoryItems:get(i):getDisplayName() ~= ('Candy Cigarette') then
-						smokable[getTableSize(smokable)] = ContainerContent:get(i)
-					end
+					smokable[getTableSize(smokable)] = ContainerContent:get(i)
 				end
 			end
 		end
